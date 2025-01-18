5e09b97e143b55127f25900e241f2a2eb946d079
Alan-232 <alanpraju4@gmail.com>
2025-01-18 22:45:09 +0530

    new

--- lib/screens/navigationbar.dart
+++ lib/screens/navigationbar.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 
-class navigation_bar extends StatelessWidget {
+class navigation_bar extends StatefulWidget {
   const navigation_bar({super.key});
 
+  @override
+  State<navigation_bar> createState() => _navigation_barState();
+}
+
+class _navigation_barState extends State<navigation_bar> {
+  int MyIndex =0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
         bottomNavigationBar: NavigationBar(
+
           height: 80,
           elevation: 0,
           selectedIndex: 0,
 
           destinations: [
           NavigationDestination(icon: Icon(Icons.home), label: 'StreakPage'),
-          NavigationDestination(icon: Icon(Icons.checklist), label: 'TaskPage'),
+          NavigationDestination(icon: Icon(Icons.checklist), label: 'task_screen'),
           NavigationDestination(icon: Icon(Icons.youtube_searched_for_outlined), label: 'WatchPage'),
           NavigationDestination(icon: Icon(Icons.person), label: 'Profile')
         ],)
