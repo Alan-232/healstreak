eb5b10d7c34794e1bcabf444b28af7a0da83920e
Alan-232 <alanpraju4@gmail.com>
2025-01-18 14:38:01 +0530

    survey

---
+++ lib/screens/survey/survey.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class survey_screen extends StatelessWidget {
+  const survey_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
